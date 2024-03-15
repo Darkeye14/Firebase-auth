2646339c244a3634ba00a973a7f662045146cd80
Gyan-Ponnappa-1 <gyanopcon@gmail.com>
2024-03-16 00:33:46 +0530

    further code

--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.firebaseauth
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,6 +36,11 @@ class MainActivity : ComponentActivity() {
                         composable("sign_in"){
                             val viewModel = viewModel<SignInViewModel>()
                             val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            val launcher = rememberLauncherForActivityResult(
+                                contract = ActivityResultContracts.StartIntentSenderForResult()) {
+
+                            }
                         }
                     }
 
